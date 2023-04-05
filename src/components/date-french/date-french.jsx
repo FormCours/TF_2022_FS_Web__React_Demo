8c64b1be9f2bbf3f5a1824f589358e62df38ad0b
Aurélien <aurelien.strimelle@bstorm.be>
2023-04-05 14:25:04 +0200

    Demo 03 - Add css option in DateFrench

--- src/components/date-french/date-french.jsx
+++ src/components/date-french/date-french.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const DateFrench = ({ date }) => {
+const DateFrench = ({ date, className, style }) => {
 
   const dateFormatted = (new Date(date)).toLocaleDateString('fr', {
     day: 'numeric',
@@ -9,12 +9,14 @@ const DateFrench = ({ date }) => {
   });
 
   return (
-    <span>{dateFormatted}</span>
+    <span className={className} style={style}>{dateFormatted}</span>
   );
 };
 
 DateFrench.propTypes = {
-  date: PropTypes.string.isRequired
+  date: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.object
 };
 
 export default DateFrench;
